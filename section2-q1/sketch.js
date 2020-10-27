69a73a2db3fc83ffa96bc0993cfda225b65bf359
nnn5757 <72908312+nnn5757@users.noreply.github.com>
2020-10-27 20:14:08 +0900

    小手調べ課題

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
+  createCanvas(200, 200)
   for(let i = 0; i < 10; i++){
+    noFill();
+    if(i < 5){
+    stroke(0, 0, 255);
+  }
+  else{
+  stroke(255, 0, 0);
+}
+  ellipse(60, 60, 10 + 10 * i);
+
     // BLANK[1]
   }
 }
